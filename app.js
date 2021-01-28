b86e8a05935bf831fa242617cc4b25df70e31fbe
Henry Kung <henrykung@live.ca>
2021-01-28 12:42:57 -0500

    remove unneeded config

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/toDoList", indexRouter);
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
